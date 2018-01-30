07af9a92567d1b0403e7143b68b91785a49016d2
clark cuadro <clarkant@hawaii.edu>
2018-01-29 14:44:25 -1000

    learning Project One
    learning Project One

---
+++ ProjectOne.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//:Variable Coding Challenge #1
+
+import UIKit
+
+var str = "firstString"
+
+var myFirstVariable:  String
+myFirstVariable = "I've created my first string"
+
+//We're going to do something with strings
+var mySecondVariable = "and I've added two strings together"
+
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+print(myThirdVariable)
